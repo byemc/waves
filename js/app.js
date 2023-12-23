4c51709e1cfa60e8e600acc19afaac0fa9fec9a4
bye <bye@byecorps.com>
2023-12-23 07:41:23 +0000

    Forgot to commit lawl

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ const player = document.getElementById("player");
 const audioSource = player.children.item(0);
 console.debug(audioSource);
 
+// Important init stuff
+
+audioSource.src = "/assets/audio/noise.wav";
+let currentStation = null;
+
 // Play/Pause button
 
 const playPauseButton = document.getElementById("playpause");
@@ -71,7 +76,7 @@ controlVolume.addEventListener("input", _ => {
 
 function setPlayPauseButtonIcon(status="stopped") {
   const statuses = {
-    stopped: "<span class='fa-fw fa-solid fa-stop'></span> Stopped",
+    stopped: "<span class='fa-fw fa-solid fa-play'></span> Stopped",
     playing: "<span class='fa-fw fa-solid fa-pause'></span> Playing",
     paused: "<span class='fa-fw fa-solid fa-play'></span> Paused",
     buffer: "<span class='fa-fw fa-solid fa-spin fa-spinner'></span> Buffering",
@@ -95,8 +100,12 @@ playPauseButton.addEventListener("click", togglePlay);
 
 // Give user feedback on buffering
 
+let noise = null;
+
 player.addEventListener('playing', function() {
   console.log('Playback started.');
+  noise.pause();
+  noise = null;
   setPlayPauseButtonIcon("playing");
 });
 
@@ -107,6 +116,10 @@ player.addEventListener('pause', function() {
 
 player.addEventListener('waiting', function() {
   console.log("Buffering...");
+  noise = new Audio("/assets/audio/noise.wav");
+  noise.play();
+  noise.volume = 0.4;
+  noise.loop = true;
   setPlayPauseButtonIcon("buffer");
 });
 
@@ -122,18 +135,49 @@ function getLocalStorageItem (item, fallback = null) {
 setVolume(getLocalStorageItem("volume", 100));
 let stationList = JSON.parse(getLocalStorageItem("stations", "[]"));
 
-// Creates stations
-function addStation(url, name="", metadata_type="icecast", azuracast_server_url="", azuracast_station_shortname="") {
+// Stations
+function addStation(url, name="", metadata_type="icecast", azuracast_server_url="", azuracast_station_shortcode="") {
   console.log("Adding station: ", {
       url,
       name,
       metadata_type,
       azuracast_server_url,
-      azuracast_station_shortname
+      azuracast_station_shortcode
   });
 
-  stationList.push({url, name, metadata_type, azuracast_server_url, azuracast_station_shortname});
+  stationList.push({url, name, metadata_type, azuracast_server_url, azuracast_station_shortcode});
   localStorage.setItem("stations", JSON.stringify(stationList));
+}
+
+function removeStation(station) {
+  console.log(stationList.indexOf(station))
+
+  if (stationList.length === 1) {
+
+    stationList = [];
+    localStorage.setItem("stations", JSON.stringify(stationList));
+    renderStationListInTuner();
+
+    return
+  }
+
+  stationList = stationList.slice(stationList.indexOf(station), 1);
+  console.log(stationList);
+  localStorage.setItem("stations", JSON.stringify(stationList));
+
+
+  renderStationListInTuner();
+}
+
+function tuneRadio(station={url:"/assets/audio/noise.wav"}) {
+  player.pause();
+  console.log(station)
+  audioSource.src = station.url;
+  currentStation = station;
+  player.load();
+  player.play();
+
+  location.hash = "#station";
 }
 
 // View-specific code
@@ -144,9 +188,16 @@ function renderStationListInTuner() {
   const tunerStationList = document.getElementById("tunerStationList");
   tunerStationList.innerText = "";
   for (let result of stationList) {
+    let station = result;
     const newStation = document.createElement("div");
     newStation.classList = "station";
-    newStation.innerHTML = `<h3 class="station_title">${result.name}</h3><button class="tune_station"><span class="fa-fw fa-solid fa-play-circle"></span> Play Station</button>`
+    newStation.innerHTML = `<h3 class="station_title">${result.name}</h3>
+<button class="tune_station"><span class="fa-fw fa-solid fa-play-circle"></span> Play Station</button>
+<button class="remove_station dangerous"><span class="fa-fw fa-solid fa-trash" title="Remove Station"></span></button>
+`
+    newStation.getElementsByClassName("tune_station")[0].onclick = _=> { tuneRadio(result) };
+    newStation.getElementsByClassName("remove_station")[0].onclick = _=> { removeStation(station) };
+
     document.getElementById("tunerStationList").appendChild(newStation);
   }
 }
@@ -165,16 +216,16 @@ document.getElementById("add_station_add_station").addEventListener("click", fun
   const audioURL = document.getElementById("add_station_url").value;
   const stationName = document.getElementById("add_station_name").value;
   const stationMetadataType = document.querySelector("input[name='add_metadata_type']:checked").value;
-  let azuracastServerURL, azuracastStationShortname;
+  let azuracastServerURL, azuracastStationshortcode;
   if (stationMetadataType === "azuracast") {
     azuracastServerURL = document.getElementById("add_station_azuracast_server").value;
-    azuracastStationShortname = document.getElementById("add_station_azuracast_shortname").value;
+    azuracastStationshortcode = document.getElementById("add_station_azuracast_shortcode").value;
   } else {
     azuracastServerURL = "";
-    azuracastStationShortname = "";
+    azuracastStationshortcode = "";
   }
 
-  addStation(audioURL, stationName, stationMetadataType, azuracastServerURL, azuracastStationShortname);
+  addStation(audioURL, stationName, stationMetadataType, azuracastServerURL, azuracastStationshortcode);
   location.hash = "#tuner";
 });
 
@@ -189,7 +240,7 @@ async function getStationsFromAzuracastServer(url) {
   for (let result of results) {
     const newStation = document.createElement("div");
     newStation.classList = "station search_result";
-    newStation.innerHTML = `<h3 class="station_title">${result.name}</h3><p class="description">${result.description}</p><button onclick="addStation('${result.default_url}', '${result.name}', 'azuracast', '${url}', ${result.shortname});" class="add_station"><span class="fa-fw fa-solid fa-plus-circle"></span> Add Station</button>`
+    newStation.innerHTML = `<h3 class="station_title">${result.name}</h3><p class="description">${result.description}</p><button onclick="addStation('${result.listen_url}', '${result.name}', 'azuracast', '${url}', '${result.shortcode}'); location.hash='#tuner';" class="add_station"><span class="fa-fw fa-solid fa-plus-circle"></span> Add Station</button>`
     document.getElementById("add_azuracast_results").appendChild(newStation);
   }
 }
@@ -213,6 +264,29 @@ const views = {
 
 const viewFunctions = { // These run when a view is loaded.
   "#tuner": renderStationListInTuner,
+  "#station": async function () {
+    const stationThingy = views['#station'];
+
+    if (!currentStation) {
+      stationThingy.innerHTML = `<span class="fa-fw fa-solid fa-warning"></span> Not tuned.`;
+      alert("You need to tune the radio first.");
+      location.hash = "#tuner";
+      return;
+    }
+
+    if (currentStation.metadata_type === "azuracast") {
+      let request = await fetch(currentStation.azuracast_server_url + `/api/station/${currentStation.azuracast_station_shortcode}`);
+      let json = await request.json();
+
+      stationThingy.innerHTML = `
+    <h2>${json.name}</h2>
+    <code>${json.shortcode}</code>
+    <p>${json.description}</p>`
+    } else {
+      stationThingy.innerHTML = "Yeah this is icecast, metadata coming soon."
+    }
+
+  }
 }
 
 function updateView() {
